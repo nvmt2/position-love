40007721594094f0688e01ca2b0caed4eff51b0e
Nguyen Van Minh Toi <toi.nguyen.vtp@enjoyworks.co.kr>
2022-02-25 17:44:32 +0700

    Add shake feature

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const baseUrlImg = './assets/';
 const imageNode = document.querySelector('.image-posture');
 const lengthArrayImage = 18;
 
+if (!('ondevicemotion' in window)) {
+  alert('Not Supported');
+}
+
 const randomIntFromInterval = (min, max) => {
   // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -12,3 +16,19 @@ const randomImage = () => {
 
   imageNode.src = `${baseUrlImg}/${randomNumber}.jpg`;
 };
+
+//listen ano to shake event
+var shakeEvent = new Shake({ threshold: 15 });
+shakeEvent.start();
+window.addEventListener(
+  'shake',
+  function () {
+    randomImage();
+  },
+  false
+);
+
+// //stop ny listening
+// function stopShake(){
+// shakeEvent.stop();
+// }
